9e2e8aa25b1c0014b03d30041b8e3afe8b9f6af3
Sun Weiguang <sunweiguang224@gmail.com>
2016-08-24 16:27:48 +0800

    分页面编译

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 1.如windows编译node-sass不成功，则将other/node-sass.zip解压后放入node_modules文件夹下。
 2.谷歌商店添加JetBrains IDE Support扩展实现文件变化自动刷新浏览器。
+3.启动方式：
+  npm start
+  npm run build
+  npm start -- --name login
+  npm run build -- --name login
--- path.js
+++ path.js
@@ -1,3 +1,9 @@
+var minimist = require('minimist'); // 获取key-value形势的参数，如 npm run build -- --name login --age 18
+
+// 分模块编译，优先级：命令行 > 指定 > 全部，
+var args = minimist(process.argv);
+var moduleName = args.name || 'login' || '*';
+
 // ************************************ 变量Path ************************************
 const Path = {
 	srcRoot: 'src',
@@ -5,13 +11,19 @@ const Path = {
 	distRoot: 'dist'
 };
 Path.src = {
-	css: Path.srcRoot + '/*(module|common)/**/css/*.scss',
-	icon: Path.srcRoot + '/*(module)/*/img/*/*', // common模块下图片是公用的，页面之间可以利用缓存，故不作处理
-	img: Path.srcRoot + '/*(module|common)/**/img/{*.png, *.jpg}',//
-	html: Path.srcRoot + '/*(module)/*/*.html',
+	css: [
+    Path.srcRoot + '/*common/css/*.scss',     // 原先写法 css: Path.srcRoot + '/*(module|common)/**/css/*.scss',
+    Path.srcRoot + '/*module/'+moduleName+'/css/*.scss'
+  ],
+	icon: Path.srcRoot + '/*module/*/img/*/*', // common模块下图片是公用的，页面之间可以利用缓存，故不作处理
+	img: [
+    Path.srcRoot + '/*common/img/{*.png, *.jpg}',
+    Path.srcRoot + '/*module/'+moduleName+'/img/{*.png, *.jpg}'
+  ],
+	html: Path.srcRoot + '/*module/'+moduleName+'/*.html',
 	js: {
 		common: Path.srcRoot + '/*common/js/*.js',	// 由nodejs负责
-		module: Path.srcRoot + '/*module/*/js/*.js'		// 由webpack负责
+		module: Path.srcRoot + '/*module/'+moduleName+'/js/*.js'		// 由webpack负责
 	},
 	generator: [
 		'src/generator/*.html',

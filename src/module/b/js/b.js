26a44b71d10d359f5407d69bcb3eb6b3f6e21f28
Sun Weiguang <sunweiguang224@gmail.com>
2016-09-05 16:31:48 +0800

    clearfix

--- src/module/b/js/b.js
+++ src/module/b/js/b.js
@@ -8,10 +8,37 @@ var util = require('util');
 import jquery from 'jquery';
 
 function Biz() {
-	var $body = $('body');
-	//var tpl = require('../tpl/demo.tpl')({a: Date.now()});
-	//var css = require('../css/demo.scss');
-	//var json = require('../json/demo.json');
+
 
 }
 new Biz();
+
+var beforeFillZero = function(value, digit){
+  value = value.toString();
+  var zeroNum = digit - value.length;
+  for(var i=0;i<zeroNum;i++){
+    value = '0' + value;
+  }
+  return value;
+};
+
+var dateFormat = function(date, format){
+  date = Object.prototype.toString.call(date) == '[object String]' ? new Date(date) : date || new Date();
+  format = Object.prototype.toString.call(format) == '[object String]' ? format : 'yyyy-MM-dd hh:mm:ss';
+  var map = {
+    'y': date.getFullYear(),
+    'M': date.getMonth() + 1,
+    'd': date.getDate(),
+    'H': date.getHours(),
+    'm': date.getMinutes(),
+    's': date.getSeconds(),
+    'S': date.getMilliseconds()
+  };
+  for(var key in map){
+    format = format.replace(new RegExp(key + '+'), function(matchValue, index, input){
+      return beforeFillZero(map[key], matchValue.length);
+    });
+  }
+  return format;
+};
+console.log(dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss SSS'));
--- src/util/js/templateHelper.js
+++ src/util/js/templateHelper.js
@@ -5,32 +5,43 @@ var template = require('tmodjs-loader/runtime');
  * 用法：{{时间戳 | dateFormat:'yyyy-MM-dd hh:mm:ss SSS'}}
  */
 template.helper('dateFormat', function (timestamp, pattern) {
-  var dateFormat = function (date, pattern) {
-    date = date || new Date();
-    date = date instanceof Date ? date : new Date(date);
-    pattern = pattern || 'yyyy-MM-dd hh:mm:ss';
+  /**
+   * 时间格式化
+   * @param date 日期对象|时间戳数字|时间戳字符串
+   * @param format 格式化字符串
+   * @returns {String}
+   * Demo: swg.dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss SSS');
+   */
+  var dateFormat = function(date, format){
+    date = Object.prototype.toString.call(date) == '[object String]' ? new Date(parseInt(date)) :
+      Object.prototype.toString.call(date) == '[object Number]' ? new Date(date) :
+      date || new Date();
+    format = Object.prototype.toString.call(format) == '[object String]' ? format : 'yyyy-MM-dd hh:mm:ss SSS';
     var map = {
-      "M+": date.getMonth() + 1, //month
-      "d+": date.getDate(), //day
-      "h+": date.getHours(), //hour
-      "m+": date.getMinutes(), //minute
-      "s+": date.getSeconds(), //second
-      "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+      'y': date.getFullYear(),
+      'M': date.getMonth() + 1,
+      'd': date.getDate(),
+      'H': date.getHours(),
+      'm': date.getMinutes(),
+      's': date.getSeconds(),
+      'S': date.getMilliseconds()
     };
-    if (/(y+)/.test(pattern)) {
-      pattern = pattern.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for(var key in map){
+      format = format.replace(new RegExp(key + '+'), function(matchValue, index, input){
+        return beforeFillZero(map[key], matchValue.length);
+      });
     }
-    if (/(S+)/.test(pattern)) {
-      pattern = pattern.replace(RegExp.$1, (date.getMilliseconds() + "").substr(3 - RegExp.$1.length));
-    }
-    for (var i in map) {
-      if (new RegExp("(" + i + ")").test(pattern)) {
-        pattern = pattern.replace(RegExp.$1, RegExp.$1.length == 1 ? map[i] : ("00" + map[i]).substr(("" + map[i]).length));
-      }
-    }
-    return pattern;
+    return format;
   };
-  return dateFormat(timestamp, pattern)
+  var beforeFillZero = function(value, digit){
+    value = value.toString();
+    var zeroNum = digit - value.length;
+    for(var i=0;i<zeroNum;i++){
+      value = '0' + value;
+    }
+    return value;
+  };
+  return dateFormat(timestamp, pattern);
 });
 
 module.exports = template;
